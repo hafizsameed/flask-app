1b3469209f1c13ed2df953c05764fcc693c57a9a
hafizsameed <hafizsameed42@gmail.com>
2020-02-19 21:23:24 +0500

    json

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask,json,jsonify 
+from flask import Flask,json 
 from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/')
 def helloWorld():
-    return jsonify(name="sameed")
+    return json_response({"name":"sameed"})
 
+def json_response(payload, status=200):
+ return (json.dumps(payload), status, {'content-type': 'application/json'})

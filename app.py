f6fd193af330a9b95decc2b1444ff7399d8c61b2
hafizsameed <hafizsameed42@gmail.com>
2020-02-19 21:57:08 +0500

    simple flask api

--- app.py
+++ app.py
@@ -7,5 +7,6 @@ CORS(app)
 def helloWorld():
     return json_response({"name":"sameed"})
 
+
 def json_response(payload, status=200):
  return (json.dumps(payload), status, {'content-type': 'application/json'})
